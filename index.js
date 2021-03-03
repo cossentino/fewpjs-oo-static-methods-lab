6085f4ea09734c4b28d292d0867b5cb3505dbe54
Ian Cossentino <icossentino@gmail.com>
2021-03-03 17:59:09 -0500

    commit tree f10dcccb824616c77b24a01ed42e5eb83dd09f9a

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+
+
+  static capitalize(str) {
+    let myString = str
+    let myString1 = myString[0].toUpperCase()
+    return `${myString1}${myString.slice(1)}`
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(str) {
+    const array = str.split(" ")
+    const badWords = ['a', 'an', 'but', 'the', 'of', 'and', 'for', 'at', 'by', 'from']
+    const titleizedArray = array.map(e => {
+      if (array.indexOf(e) === 0 || !badWords.includes(e)) {
+        return Formatter.capitalize(e)
+      } else {
+        return e
+      }
+    })
+    return titleizedArray.join(" ")
+  }
+
+  
+
+
+
 }
